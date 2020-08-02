82e3391843dc35017accdcf11cad817a80408b5f
subhchatu <subhchatu@gmail.com>
2020-08-02 21:37:19 +0530

    Installing Script

---
+++ install.sh
@@ -0,0 +1,15 @@
+mkdir $HOME/foldersorter
+
+cp ./bin/sorter.py $HOME/foldersorter/
+cp ./bin/adder.py $HOME/foldersorter/
+cp ./bin/extensionbase.py $HOME/foldersorter/
+cp ./bin/foldercreator.py $HOME/foldersorter/
+cp ./bin/cutfunc.py $HOME/foldersorter/
+cp ./bin/pathhelp.py $HOME/foldersorter/
+cp ./bin/main.py $HOME/foldersorter/
+cp ./bin/Sortergui.py $HOME/foldersorter/
+cp ./bin/pathbase.txt $HOME/foldersorter/
+cp ./bin/readme.md $HOME/foldersorter/
+cp ./bin/launch.sh $HOME/foldersorter/
+
+
